bb8d1d9e400bb6390a3087d29422a86c0b07d8f3
bap2494 <brian_a_pruitt@homedepot.com>
2025-02-28 11:59:34 -0500

    added collision

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -28,9 +29,16 @@ def main():
                 return
         
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
